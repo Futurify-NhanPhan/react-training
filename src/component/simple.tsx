5c2e4103022edb5f4195083c782c1d34c46676e1
NhanPhan <NhanPhan@F14-NHANPHAN>
2020-02-13 13:54:39 +0700

    install moment

--- src/component/simple.tsx
+++ src/component/simple.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import moment from "moment";
 
 type State = {};
 type Props = {};
 
 export default class Simple extends React.Component<Props, State> {
   render() {
-    return <div>Hello everyone, today is {new Date().toUTCString()}</div>;
+    return (
+      <div>
+        Hello everyone, today is{" "}
+        {moment(new Date().toUTCString()).format("DD-MM-YYYY")}
+      </div>
+    );
   }
 }
