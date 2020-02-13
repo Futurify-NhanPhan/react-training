257f6e9322fb10c230eef7c34ed619b6428a086b
NhanPhan <NhanPhan@F14-NHANPHAN>
2020-02-13 17:05:44 +0700

    React router

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import logo from "./logo.svg";
 import "./App.css";
 import Simple from "./component/Simple";
 import { Message } from "./component/Message";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./component/Home";
 
 const App = () => {
   return (
@@ -20,8 +22,31 @@ const App = () => {
         >
           Learn React
         </a>
-        <Simple></Simple>
-        <Message></Message>
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/simple">Simple</Link>
+                </li>
+              </ul>
+            </nav>
+
+            {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+            <Switch>
+              <Route path="/simple">
+                <Simple />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
       </header>
     </div>
   );
---
+++ src/component/Home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+type State = {};
+type Props = {};
+
+export default class Home extends React.Component<Props, State> {
+  render() {
+    return <div>This is home component</div>;
+  }
+}
